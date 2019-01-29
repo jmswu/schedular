a8a8137652aed173d68645274c300db487928fcd
jmswu <wu.zhuhua@gmail.com>
2019-01-30 08:15:13 +1300

    Add files via upload

--- schedular.c
+++ schedular.c
@@ -1,68 +1,81 @@
 #include "schedular.h"
 #include <stdio.h>
 
-// an task array to store all the task, and their params
-// and the task schedular will read this list the run
-// task according their params
-static task_t task_list[TASK_SIZE];
 
-static get_ticks_t schedular_get_ticks;
 
-void schedular_init(get_ticks_t get_ticks){
+schedular_t schedular;
+
+void schedular_init(get_ticks_t get_ticks) {
     uint16_t i;
-    for (i = 0; i < TASK_SIZE; i++){
-        task_list[i].task = 0;
-        task_list[i].ticks_interval = 0;
-        task_list[i].ticks_previous = 0;
+    for (i = 0; i < TASK_SIZE; i++) {
+        schedular.task_list[i].task = 0;
+        schedular.task_list[i].ticks_interval = 0;
+        schedular.task_list[i].ticks_previous = 0;
+        schedular.task_count = 0;
     }
-    if (get_ticks != 0) schedular_get_ticks = get_ticks;
+    if (get_ticks != 0) schedular.get_ticks = get_ticks;
 }
 
-void schedular_add(func_t task, uint32_t interval){
+void schedular_add(func_t task, uint32_t interval) {
     // find an empty task
     uint16_t i;
-    for (i = 0; i < TASK_SIZE; i++){
+    for (i = 0; i < TASK_SIZE; i++) {
         // if an empty location is found, add the new task to it
-        if (task_list[i].task == 0){
-            task_list[i].ticks_interval = interval;
-            task_list[i].task = task;
+        if (schedular.task_list[i].task == 0) {
+            schedular.task_list[i].ticks_interval = interval;
+            schedular.task_list[i].task = task;
+            schedular.task_list[i].ticks_previous = schedular.get_ticks();
+            schedular.task_count++;
             return;
         }
     }
 }
 
-void schedular_run(void){
+void schedular_remove(func_t task) {
+    // find the task
+    uint16_t i;
+    for (i = 0; i < TASK_SIZE; i++) {
+        // a task is found
+        // remove the task from the list
+        if (schedular.task_list[i].task == task) {
+            schedular.task_list[i].task = 0;
+            schedular.task_count--;
+            return;
+        }
+    }
+}
+
+void schedular_run(void) {
     uint16_t i = 0;
     uint32_t ticks = 0;
     // loop through all the task
-    for (i = 0; i < TASK_SIZE; i++){
+    for (i = 0; i < schedular.task_count; i++) {
         // find a task
-        if (task_list[i].task != 0){
+        if (schedular.task_list[i].task != 0) {
             // if task's timer is expired, run the task
-            ticks = schedular_get_ticks();
-            if ((ticks - task_list[i].ticks_previous) >= task_list[i].ticks_interval){
-                task_list[i].ticks_previous = ticks;
-                task_list[i].task();
+            ticks = schedular.get_ticks();
+            if ((ticks - schedular.task_list[i].ticks_previous) >= schedular.task_list[i].ticks_interval) {
+                schedular.task_list[i].ticks_previous = ticks;
+                // if task return 0, this mean the task want to remove
+                // itself from the schedular
+                if (schedular.task_list[i].task() == 0){
+                    schedular_remove(schedular.task_list[i].task);
+                }
             }
         }
     }
 }
 
-void schedular_print_task(void){
+void schedular_print_task(void) {
     uint16_t i = 0;
-    for (i = 0; i < TASK_SIZE; i++){
-        printf("task_%d addr        : %08X\n", i, (uint16_t)task_list[i].task);
-        printf("task_%d interval    : %08X\n", i, (uint16_t)task_list[i].ticks_interval);
-        printf("task_%d last        : %08X\n", i, (uint16_t)task_list[i].ticks_previous);
+    for (i = 0; i < TASK_SIZE; i++) {
+        printf("task_%d addr        : %04X\n", i, (uint16_t) schedular.task_list[i].task);
+        printf("task_%d interval    : %08X\n", i, (uint16_t) schedular.task_list[i].ticks_interval);
+        printf("task_%d last        : %08X\n", i, (uint16_t) schedular.task_list[i].ticks_previous);
         printf("\n");
     }
 }
 
-uint16_t schedular_get_free_task(void){
-    uint16_t free_task = 0;
-    uint16_t i = 0;
-    for(i = 0; i < TASK_SIZE; i++){
-        if (task_list[i].task == 0U) free_task++;
-    }
-    return free_task;
+uint16_t schedular_get_free_task(void) {
+    return TASK_SIZE - schedular.task_count;
 }
--- schedular.h
+++ schedular.h
@@ -13,37 +13,57 @@ extern "C" {
 #endif
 
 #include <stdint.h>
-    
-// define the maximum number of task that is available
+
+    // define the maximum number of task that is available
 #define TASK_SIZE     (10U)
-    
+
     // function pointer use in task
-    typedef void (*func_t)(void);
-    typedef uint32_t (*get_ticks_t)(void);
-    
+    typedef uint16_t (*func_t)(void);
+    typedef uint32_t(*get_ticks_t)(void);
+
     // task struct for use in schedular
-    typedef struct{
-        uint32_t ticks_interval;    // task interval, 
-        uint32_t ticks_previous;    // previous ticks
-        func_t task;                // function pointer
-    }task_t;
-    
+
+    typedef struct {
+        uint32_t ticks_interval; // task interval, 
+        uint32_t ticks_previous; // previous ticks
+        func_t task; // function pointer
+    } task_t;
+
+    typedef struct {
+        // will be point to external getTicks() function
+        get_ticks_t get_ticks; 
+        
+        // an task array to store all the task, and their params
+        // and the task schedular will read this list the run
+        // task according their params
+        task_t task_list[TASK_SIZE];
+        
+        // the number of tasks
+        uint16_t task_count;
+    } schedular_t;
+
+    extern schedular_t schedular;
+
     // initial schedular, this must run first
     // @param task      - pointer to a task than return system ticks
     void schedular_init(get_ticks_t get_ticks);
-    
+
     // add a task to the schedular
     // @param task      - the task to add
     // @param interval  - how often the task need to run 
     void schedular_add(func_t task, uint32_t interval);
-    
+
+    // remove a task from the schedular
+    // @param task      - the task to be removed
+    void schedular_remove(func_t task);
+
     // this run the schedular, this should be in the main loop
     void schedular_run(void);
-    
+
     void schedular_print_task(void);
-    
+
     uint16_t schedular_get_free_task(void);
-    
+
 
 
 #ifdef	__cplusplus
